c0409f3eb84d3d41a9a1fbea304c95dbef353a56
zeedraw <454371744@qq.com>
2016-08-21 21:37:54 +0800

    更改了返回bug

--- ourapp/app/src/main/java/com/example/administrator/ourapp/MySetting.java
+++ ourapp/app/src/main/java/com/example/administrator/ourapp/MySetting.java
@@ -1,16 +1,41 @@
 package com.example.administrator.ourapp;
 
+import android.content.ComponentName;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 /**
  * Created by Administrator on 2016/8/21.
  */
 public class MySetting extends AppCompatActivity {
+    Button rt_button;
+    TextView title;
     @Override
     protected void onCreate( Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.setting);
+        initWidget();
 
+    }
+
+    private void initWidget()
+    {
+        rt_button=(Button)findViewById(R.id.lbt);
+        title=(TextView)findViewById(R.id.title);
+        rt_button.setText("返回");
+        rt_button.setVisibility(View.VISIBLE);
+        title.setText("设置");
+
+        rt_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+
+                MySetting.this.finish();
+            }
+        });
     }
 }
