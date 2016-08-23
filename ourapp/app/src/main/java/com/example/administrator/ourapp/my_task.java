a6e25c743dc599c0ddd0b246a26b7e56bf39fab9
songlongze <673396701@qq.com>
2016-08-23 09:27:05 +0800

    增加了我的任务界面 colors文件增加了颜色资源 增加了slidingtablayout

---
+++ ourapp/app/src/main/java/com/example/administrator/ourapp/my_task.java
@@ -0,0 +1,41 @@
+package com.example.administrator.ourapp;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.view.PagerAdapter;
+import android.support.v4.view.ViewPager;
+import android.support.v7.app.ActionBarActivity;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.LinearLayout;
+import android.support.v4.app.Fragment;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dell-pc on 2016/8/21.
+ */
+public class my_task extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.my_task);
+        ViewPager pager = (ViewPager) findViewById(R.id.pager);
+
+        SlidingTabLayout tab = (SlidingTabLayout) findViewById(R.id.tab);
+
+        List<Fragment> frg = new ArrayList<Fragment>();
+        frg.add(new Frg_task_applying());
+        frg.add(new Frg_task_ing());
+        frg.add(new Frg_task_completed());
+        task_fragmentAdapter adapter = new task_fragmentAdapter(getSupportFragmentManager(), frg);
+
+        pager.setAdapter(adapter);
+        tab.setCustomTabView(R.layout.tab, 0);
+        tab.setViewPager(pager);
+    }//onCreate
+
+
+}
---
+++ ourapp/app/src/main/java/com/example/administrator/ourapp/task_fragmentAdapter.java
@@ -0,0 +1,39 @@
+package com.example.administrator.ourapp;
+
+import android.support.v4.app.Fragment;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentPagerAdapter;
+
+import java.util.List;
+
+/**
+ * Created by Administrator on 2016/8/17.
+ */
+public class task_fragmentAdapter extends FragmentPagerAdapter {
+
+    private List<Fragment> mFragments;
+    private String mTabTitle[] = new String[]{"申请中","进行中","已完成"};
+
+    public task_fragmentAdapter(FragmentManager fm, List<Fragment> fragments) {
+        super(fm);
+        mFragments=fragments;
+    }
+
+    @Override
+    public Fragment getItem(int arg0) {
+
+        return mFragments.get(arg0);
+    }
+
+    @Override
+    public int getCount() {
+
+        return mFragments.size();
+    }
+
+    @Override
+    public CharSequence getPageTitle(int position) {
+        return  mTabTitle[position];
+    }
+
+}
