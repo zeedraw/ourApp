5a7fc7dfa1a3b15c51bfe90c61929153a998ab22
zeedraw <454371744@qq.com>
2016-09-19 21:05:09 +0800

    完成首页 教育类的下拉刷新 下滑加载

--- ourapp/app/src/main/java/com/example/administrator/ourapp/Frag_edu.java
+++ ourapp/app/src/main/java/com/example/administrator/ourapp/Frag_edu.java
@@ -1,70 +1,217 @@
 package com.example.administrator.ourapp;
 
-import android.content.ComponentName;
-import android.content.Intent;
 import android.os.Bundle;
-import android.support.v4.app.Fragment;
+import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.AdapterView;
-import android.widget.BaseAdapter;
 import android.widget.ListView;
-import android.widget.TextView;
+
+import com.example.administrator.ourapp.pulltorefresh.PullToRefreshBase;
+import com.example.administrator.ourapp.pulltorefresh.PullToRefreshListView;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.List;
+
+import cn.bmob.v3.BmobQuery;
+import cn.bmob.v3.datatype.BmobDate;
+import cn.bmob.v3.exception.BmobException;
+import cn.bmob.v3.listener.FindListener;
 
 /**
  * Created by Administrator on 2016/8/19.
  */
-public class Frag_edu extends Fragment {
-    private BaseAdapter adapter;
-    private ListView listView;
-
-    @Override
-    public void onCreate( Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-
-    }
+public class Frag_edu extends ProgressFragment {
+    private SimpleDateFormat mDateFormat = new SimpleDateFormat("MM-dd HH:mm");
+    private boolean mIsStart = true;
+    private PullToRefreshListView mPullListView;
+    private View mContentView;
+    private List<Mission> mlist;
+    private MissionAdapter mAdapter;
+    private ListView mlistview;
+    private int limit=7;//每页的长度
+    private int curPage=0;//当前页
+    private String lastTime;
 
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,  Bundle savedInstanceState) {
-        View rootView=inflater.inflate(R.layout.frag_edu,container,false);
-        listView=(ListView)rootView.findViewById(R.id.listview_edu);
-        adapter=new BaseAdapter() {
-            @Override
-            public int getCount() {
-                return 14;//测试个数
-            }
+        mPullListView = new PullToRefreshListView(getContext());
+        mPullListView.setPullLoadEnabled(false);
+        mPullListView.setScrollLoadEnabled(true);
 
-            @Override
-            public Object getItem(int i) {
-                return null;
-            }
 
-            @Override
-            public long getItemId(int i) {
-                return i;
-            }
-
-            @Override
-            public View getView(int i, View view, ViewGroup viewGroup) {
-                view=LayoutInflater.from(getContext()).inflate(R.layout.mission_abstract,null);
-
-                return view;//要添加的组件
-            }
-        };
-
-        listView.setAdapter(adapter);
-        //listview的监听事件
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
-                ComponentName comp=new ComponentName(getActivity(),MissionInfo.class);
-                Intent intent=new Intent();
-                intent.setComponent(comp);
-                startActivity(intent);
-            }
-        });
-        return rootView;
+        // mContentView=inflater.inflate(R.layout.frag_edu,container,false);
+        //mlistview=(ListView)mContentView.findViewById(R.id.listview_edu);
+        return inflater.inflate(R.layout.fragment_progress,container,false);
 
     }
+
+    @Override
+    public void onActivityCreated(@Nullable Bundle savedInstanceState) {
+        super.onActivityCreated(savedInstanceState);
+        //初始化数据
+        setContentView(mPullListView);
+        setEmptyText("no data");
+        setContentShown(false);
+            BmobQuery<Mission> query=new BmobQuery<Mission>();
+            query.addWhereEqualTo("tag","教育");
+            query.order("-createdAt");
+            query.include("pub_user[name].userimage");
+            query.setLimit(limit);
+            query.setCachePolicy(BmobQuery.CachePolicy.CACHE_ELSE_NETWORK);
+            query.findObjects(new FindListener<Mission>() {
+                @Override
+                public void done(List<Mission> list, BmobException e) {
+                    if (e==null)
+                    {
+                        Log.i("z","查询数据成功");
+                        if (list.size()!=0) {
+                            curPage++;
+                            lastTime = list.get(list.size()-1).getCreatedAt();
+                            mlist = new ArrayList<Mission>(list);
+                            mlistview = mPullListView.getRefreshableView();
+                            mAdapter = new MissionAdapter(getContext(), R.layout.mission_abstract, mlist);
+                            mlistview.setAdapter(mAdapter);
+                            setContentShown(true);
+                        }
+                        else
+                        {
+                            setContentEmpty(true);
+                        }
+                    }
+                    else
+                    {
+                        Log.i("z","查询数据失败");
+                    }
+                }
+            });
+
+        mPullListView.setOnRefreshListener(new PullToRefreshBase.OnRefreshListener<ListView>() {
+            @Override
+            public void onPullDownToRefresh(PullToRefreshBase<ListView> refreshView) {
+                    mIsStart=true;
+                        queryData();
+
+
+            }
+
+            @Override
+            public void onPullUpToRefresh(PullToRefreshBase<ListView> refreshView) {
+                    mIsStart=false;
+                        queryData();
+            }
+        });
+
+    }
+
+    private void queryData()
+    {
+        BmobQuery<Mission> query=new BmobQuery<Mission>();
+        query.order("-createdAt");
+        query.addWhereEqualTo("tag","教育");
+        query.include("pub_user[name].userimage");
+        if (mIsStart)//true 下拉刷新
+        {
+            query.setSkip(0);
+        }
+        else // 上拉加载
+        {
+            Date date = null;
+            SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+            try {
+                date = sdf.parse(lastTime);
+            } catch (ParseException e) {
+                e.printStackTrace();
+            }
+            // 只查询小于等于最后一个item发表时间的数据
+            query.addWhereLessThanOrEqualTo("createdAt", new BmobDate(date));
+            // 跳过之前页数并去掉重复数据
+            query.setSkip(curPage * limit+1);
+        }
+        query.setLimit(limit);
+        query.findObjects(new FindListener<Mission>() {
+            @Override
+            public void done(List<Mission> list, BmobException e) {
+                if (e==null)
+                {
+                    if (list.size()>0)
+                    {
+                        if (mIsStart)
+                        {
+                            curPage=0;
+                            mlist.clear();
+                            lastTime = list.get(list.size()-1).getCreatedAt();
+                        }
+                        for (Mission mission:list)
+                        {
+                            mlist.add(mission);
+                        }
+                        curPage++;
+                        mPullListView.setHasMoreData(true);
+                    }
+                    else {
+                        mPullListView.setHasMoreData(false);
+                    }
+                    mAdapter.notifyDataSetChanged();
+                    mPullListView.onPullDownRefreshComplete();
+                    mPullListView.onPullUpRefreshComplete();
+                    setLastUpdateTime();
+
+                }
+            }
+        });
+    }
+
+    private void setLastUpdateTime() {
+        String text = formatDateTime(System.currentTimeMillis());
+        mPullListView.setLastUpdatedLabel(text);
+    }
+
+    private String formatDateTime(long time) {
+        if (0 == time) {
+            return "";
+        }
+
+        return mDateFormat.format(new Date(time));
+    }
+//    private class MyAsyncTask extends AsyncTask<Void,Void,Void>
+//    {
+//        @Override
+//        protected Void doInBackground(Void... voids) {
+//            BmobQuery<Mission> query=new BmobQuery<Mission>();
+//            query.addWhereEqualTo("tag","教育");
+//            query.include("pub_user[name|userimage]");
+//            query.setLimit(7);
+//            query.setCachePolicy(BmobQuery.CachePolicy.CACHE_ELSE_NETWORK);
+//            query.findObjects(new FindListener<Mission>() {
+//                @Override
+//                public void done(List<Mission> list, BmobException e) {
+//                    if (e==null)
+//                    {
+//                        Log.i("z","查询数据成功");
+//                        mlist=new ArrayList<Mission>(list);
+//                    }
+//                    else
+//                    {
+//                        Log.i("z","查询数据失败");
+//                    }
+//                }
+//            });
+//
+//            return null;
+//        }
+//
+//        @Override
+//        protected void onPostExecute(Void aVoid) {
+//            mAdapter=new MissionAdapter(getContext(),R.id.mission_abs,mlist);
+//            mlistview.setAdapter(mAdapter);
+//            setContentShown(true);
+//            super.onPostExecute(aVoid);
+//
+//        }
+//    }
 }
---
+++ ourapp/app/src/main/java/com/example/administrator/ourapp/imageloader/AsyncImageLoader.java
@@ -0,0 +1,134 @@
+package com.example.administrator.ourapp.imageloader;
+
+import java.io.InputStream;
+import java.lang.ref.SoftReference;
+import java.net.URL;
+import java.util.HashMap;
+
+import android.graphics.drawable.BitmapDrawable;
+import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.os.Handler;
+import android.os.Message;
+import android.util.Log;
+
+
+/**
+ * 异步加载图片类，内部有缓存，可以通过后台线程获取网络图片。首先生成一个实例，并调用loadDrawableByTag方法来获取一个Drawable对象
+ */
+public class AsyncImageLoader {
+
+    /**
+     * 使用软引用SoftReference，可以由系统在恰当的时候更容易的回收
+     */
+    private HashMap<String, SoftReference<Drawable>> imageCache;
+
+
+    public AsyncImageLoader(){
+        imageCache = new HashMap<String, SoftReference<Drawable>>();
+    }
+
+
+
+
+    /**
+     * 通过传入的TagInfo来获取一个网络上的图片
+     * @param tag TagInfo对象，保存了position、url和一个待获取的Drawable对象
+     * @param callback ImageCallBack对象，用于在获取到图片后供调用侧进行下一步的处理
+     * @return drawable 从网络或缓存中得到的Drawable对象，可为null，调用侧需判断
+     */
+    public Drawable loadDrawableByTag(final TagInfo tag, final ImageCallBack callback){
+        Drawable drawable;
+
+        /**
+         * 先在缓存中找，如果通过URL地址可以找到，则直接返回该对象
+         */
+        if(imageCache.containsKey(tag.getUrl())){
+            drawable = imageCache.get(tag.getUrl()).get();
+            if(null!=drawable){
+                return drawable;
+            }
+        }
+
+
+
+            /**
+             * 用于在获取到网络图片后，保存图片到缓存，并触发调用侧的处理
+             */
+            final Handler handler = new Handler() {
+
+                @Override
+                public void handleMessage(Message msg) {
+
+                    TagInfo info = (TagInfo) msg.obj;
+                    imageCache.put(info.url, new SoftReference<Drawable>(info.drawable));
+                    callback.obtainImage(info);
+
+                    super.handleMessage(msg);
+                }
+
+            };
+
+
+            /**
+             * 如果在缓存中没有找到，则开启一个线程来进行网络请求
+             */
+            new Thread(new Runnable() {
+
+                @Override
+                public void run() {
+
+                    TagInfo info = getDrawableIntoTag(tag);
+                    Message msg = new Message();
+                    msg.what = 0;
+                    msg.obj = info;
+                    handler.sendMessage(msg);
+                }
+            }).start();
+
+            return null;
+
+    }
+
+
+
+    /**
+     * 通过传入的TagInfo对象，利用其URL属性，到网络请求图片，获取到图片后保存在TagInfo的Drawable属性中，并返回该TagInfo
+     * @param info TagInfo对象，需要利用里面的url属性
+     * @return TagInfo 传入的TagInfo对象，增加了Drawable属性后返回
+     */
+    public TagInfo getDrawableIntoTag(TagInfo info){
+        URL request;
+        InputStream input;
+        Drawable drawable = null;
+
+        try{
+            request = new URL(info.getUrl());
+            input = (InputStream)request.getContent();
+            drawable = Drawable.createFromStream(input, "src"); // 第二个属性可为空，为DEBUG下使用，网上的说明
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+        info.drawable = drawable;
+        return info;
+    }
+
+
+
+    /**
+     * 获取图片的回调接口，里面的obtainImage方法在获取到图片后进行调用
+     */
+    public interface ImageCallBack{
+        /**
+         * 获取到图片后在调用侧执行具体的细节
+         * @param info TagInfo对象，传入的info经过处理，增加Drawable属性，并返回给传入者
+         */
+        public void obtainImage(TagInfo info);
+    }
+
+
+
+}
+
